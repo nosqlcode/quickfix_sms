9e4e29f050ee58adc91efd5203ee50bacc2ee115
Thomas Silva <thomas@Thomas.local>
2016-03-05 01:35:15 -0700

    basic api endpoint for corrections

--- index.js
+++ index.js
@@ -13,6 +13,33 @@ app.use(bodyParser.json());
 app.post('/sms', require('./sms-handler'));
 
 
+var Correction = mongoose.model('Correction', {
+    citationNumber: String,
+    defendant: String,
+    type: String,
+    fee: Number,
+    timeStamp: Date,
+    insuranceProvider: String,
+    file: String,
+    message: String
+});
+
+app.get('/corrections', function(req, resp) {
+
+    var page = req.query.page || 1;
+    var perPage = req.query.perPage || 10;
+
+    var skip = perPage * (page - 1);
+
+    Correction.find({}, null,
+        {skip: skip, limit: perPage, sort: {timeStamp: 1}},
+        function(error, corrections) {
+
+        resp.send(corrections);
+    });
+});
+
+
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log('listening on: ' + port);
